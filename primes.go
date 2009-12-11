c048fc05cb8e41c3bd0fa1f0d483333b24f3e323
jbrownlee <jason.brownlee05@gmail.com>
2009-12-11 12:03:16 +1100

    added primes

---
+++ primes.go
@@ -0,0 +1,55 @@
+// 
+// filter primes, demo using channels and go-routines
+// from: http://golang.org/doc/go_tutorial.html
+
+package main
+
+import "fmt"
+
+// generates numbers and feeds them into a channel whcih is returned
+func generate() chan int {
+	ch := make(chan int);
+	// inline fucntion as a goroutine
+	go func() {
+		for i:= 2; ; i++ {
+			ch <- i
+		}
+	}();
+	return ch;
+}
+
+func filter(in chan int, prime int) chan int {
+	out := make(chan int);
+	// inline function as a goroutine
+	go func() {
+		for {
+			if i:= <-in; i%prime != 0 {
+				out <- i;
+			}
+		}
+	}();
+	return out;
+}
+
+// sieve function, returns a channel for ints
+func sieve() chan int {
+	out := make(chan int);
+	// inline function as a goroutine
+	go func() {
+		ch := generate();
+		for {
+			prime := <-ch;
+			out <- prime;
+			ch = filter(ch, prime);
+		}
+	}();
+	return out;
+}
+
+// entry point, runs forever
+func main() {
+	primes := sieve();
+	for {
+		fmt.Println(<-primes);
+	}
+}
