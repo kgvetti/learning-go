c3366f68dc2af7058ea81ad64beb972bee18da69
jbrownlee <jason.brownlee05@gmail.com>
2009-12-15 17:00:01 +1100

    more work on ga

--- geneticalgorithm.go
+++ geneticalgorithm.go
@@ -16,8 +16,9 @@ import (
 	"fmt"; 
 	"rand";
 	// "math";
+	"container/list";
 	"time";
-) 
+)
 
 
 // algorithm configuration
@@ -30,17 +31,28 @@ const (
 	AlgorithmNumGenerations = 100;
 )
 
+type Solution struct {
+	bitstring []bool;
+	score float;
+}
 
-func NewRandomBitstring() *[]bool {
-	var bitstring []bool = make([]bool, 100);
+
+func NewRandomSolution() *Solution {
+	s := new(Solution);
+	s.bitstring = make([]bool, ProblemNumBits);
+	s.score = 0;
 	// populate with random values
-	for i:=0; i<len(bitstring); i++ {
+	for i:=0; i<len(s.bitstring); i++ {
 		// rand in [0,1]
 		if rand.Intn(2) == 1 {
-			bitstring[i] = true;
+			s.bitstring[i] = true;
 		}
 	}
-	return &bitstring;
+	return s;
+}
+
+func (s *Solution) String() string {
+	return fmt.Sprintf("(%v) [%v]", s.score, BitstringToString(&s.bitstring));
 }
 
 func BitstringToString(bitstring *[]bool) (s string) {
@@ -55,16 +67,79 @@ func BitstringToString(bitstring *[]bool) (s string) {
 	return s;
 }
 
+func OneMax(bitstring *[]bool) (score float) {
+	for _, v := range *bitstring {
+		if v { score += 1.0; }
+	}
+	return score;
+}
+
+func Fitness(s *Solution) {
+	s.score = OneMax(&s.bitstring);
+}
+
+func InitializePopulation() (pop *list.List) {
+	population := list.New();
+	
+	c := make(chan *Solution, PopulationSize); // with buffer
+	// create all of the population in parallel
+	for i:=0; i<PopulationSize; i++ {
+		go func() {c<-NewRandomSolution();}();
+	}
+	// barrier to collect results
+	for i:=0; i<PopulationSize; i++ {
+		population.PushBack(<-c);
+	}
+	
+	return population;
+}
+
+func EvaluatePopulation(pop *list.List) (best *Solution) {	
+	c := make(chan *Solution, PopulationSize); // with buffer
+	// evaluate the population in parallel
+	for e := pop.Front(); e != nil; e = e.Next() {
+		var s = e.Value.(*Solution);
+		go func() {Fitness(s);c<-s}();
+	}
+	// barrier waiting for all evaluations to finish, locate best result
+	for i:=0; i<PopulationSize; i++ {
+		var s = <-c;
+		if best==nil || s.score>best.score {
+			best = s;
+		} 
+	}
+	
+	return best;
+}
+
+func Evolve() (best *Solution){
+	// initialize the population
+	population := InitializePopulation();	
+	// evolve the population in a sequence
+	for i:=0; i<AlgorithmNumGenerations; i++ {
+		// evaluate
+		var s = EvaluatePopulation(population);
+		if best==nil || s.score>best.score {
+			best = s;
+		}
+		// select
+		
+		// reproduce
+		
+		// replace
+	}
+	
+	return best;
+}
 
 // entry point
 func main() {
-	fmt.Printf("Starting...\n");	
 	// set the random seed
 	seed := time.LocalTime().Seconds();
 	rand.Seed(seed);
 	// run the optimization and get the best solution
-	// TODO
-	best := NewRandomBitstring();
+	fmt.Printf("Starting...[seed=%v]\n", seed);	
+	best := Evolve();
 	// display the result
-	fmt.Printf("Finished!\nThe Best solution is: %v\n", BitstringToString(best));
+	fmt.Printf("Finished!\nThe best solution is: %v\n", best);
 }
